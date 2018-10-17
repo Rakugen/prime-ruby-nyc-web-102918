f122033e5c5a639c34afa17a77e1f9285c8e1ff1
Simon Lee <simonlee0306@gmail.com>
2018-10-17 20:26:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  x = 0
+#  if num == 0 || num == 1
+#    return false
+#  end
+  while x < num
+    if num % x == 0
+      return false
+    end
+    x += 1
+  end
+  return true
+end
